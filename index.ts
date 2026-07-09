b87389bda93457ff9f7d3b8d34f4192522be7b6b
Mojolaoluwa Gafar <Mojolaoluwa1212@gmail.com>
2026-07-09 13:53:05 +0100

    dep dep

--- index.ts
+++ index.ts
@@ -13,25 +13,25 @@ const app : Application = express()
 
 
 app.use(express.json())
-const allowedOrigins = [
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "https://medi-bridge-client.vercel.app"
-]
+// const allowedOrigins = [
+//     "http://localhost:5173",
+//     "http://localhost:5174",
+//     "https://medi-bridge-client.vercel.app"
+// ]
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
-
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.get("/", (req : Request , res : Response)=>{  
     res.status(200).json({ success : true, message : "MediBridge Server running..."})
 })
